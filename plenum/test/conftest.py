22bee540c2232d2e04586c7a493ad25ad2a50a2c
Ian Costanzo <ian@anon-solutions.ca>
2021-01-20 09:19:30 -0800

    Fix fixture
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- plenum/test/conftest.py
+++ plenum/test/conftest.py
@@ -335,7 +335,7 @@ def _tdir(tdir_fact):
     basename = ''
     print("basetmp:", tdir_fact.getbasetemp())
     print("basepath:", os.path.normpath(basename))
-    print("parent:", (tdir_fact.getbasetemp() / basename).resolve().parent)
+    print("parent:", (tdir_fact.getbasetemp() / os.path.normpath(basename)).resolve().parent)
     return tdir_fact.mktemp(basename).strpath
 
 
