5972a8103dbc7c4a46dc80b398f67e502a8ea6c2
Ian Costanzo <ian@anon-solutions.ca>
2021-01-26 09:54:01 -0800

    Initial version of GHA for plenum release
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- build-scripts/ubuntu-1604/install-os-dependencies.sh
+++ build-scripts/ubuntu-1604/install-os-dependencies.sh
@@ -5,20 +5,17 @@ apt-get update -y && apt-get install -y \
         ca-certificates
 
 apt-get update -y && apt-get install -y \
-    # common stuff
         git \
         wget \
         unzip \
         python3.5 \
         python3-pip \
         python3-venv \
-    # fpm
         ruby \
         ruby-dev \
         rubygems \
         gcc \
         make \
-    # rocksdb python wrapper
         libbz2-dev \
         zlib1g-dev \
         liblz4-dev \
